890a5ad85a0f01d32226430c64c260095ae61c98
chen-shujun23 <c.shujun23@gmail.com>
2024-03-29 00:42:48 +0800

    Cleaned up client folder.
    Removed old components and added two new components ProjectDataForm and PDFUpload

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,40 +1,25 @@
 import React, { useState } from "react";
-import SearchSection from "./components/SearchSection";
-import ImageSection from "./components/ImageSection";
-import ImageModal from "./components/ImageModal";
+import ProjectDataForm from "./components/ProjectDataForm";
+import PDFUpload from "./components/PDFUpload";
 
 export const StateContext = React.createContext();
 
 const App = () => {
-  const [images, setImages] = useState([]);
-  const [value, setValue] = useState("");
-  const [error, setError] = useState(null);
-  const [uploadedImage, setUploadedImage] = useState("");
-  const [modalOpen, setModalOpen] = useState(false);
+  const [projectData, setProjectData] = useState({
+    location: "",
+    size: "",
+    typology: "",
+    programs: "",
+    description: "",
+  });
 
-  const state = {
-    images,
-    setImages,
-    value,
-    setValue,
-    error,
-    setError,
-    uploadedImage,
-    setUploadedImage,
-    modalOpen,
-    setModalOpen,
-  };
+  const state = { projectData, setProjectData };
 
   return (
     <div className="app">
       <StateContext.Provider value={state}>
-        <SearchSection />
-        <ImageSection />
-        {modalOpen && (
-          <div className="overlay">
-            <ImageModal />
-          </div>
-        )}
+        <ProjectDataForm />
+        <PDFUpload />
       </StateContext.Provider>
     </div>
   );
